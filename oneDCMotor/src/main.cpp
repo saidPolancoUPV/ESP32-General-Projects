045e272186b5befe2ab0d8dc44e252b669590a39
saidPolancoUPV <spolancom@upv.edu.mx>
2022-09-19 13:02:16 -0500

    cambios en cmotor y control motor

--- oneDCMotor/src/main.cpp
+++ oneDCMotor/src/main.cpp
@@ -1,69 +1,27 @@
 #include <Arduino.h>
+#include "CMotor.h"
 
-// Motor A
-int motor1Pin1 = 27; 
-int motor1Pin2 = 26; 
-int enable1Pin = 14;
 
-int motor2Pin1 = 33;
-int motor2Pin2 = 32;
-int enable2Pin = 25;
-
-// Setting PWM properties
-const int freq = 30000;
-const int pwmChannel = 0;
-const int resolution = 8;
-int dutyCycle = 200;
+CMotor motor(30000, 0, 8, 27, 26, 14);
+CMotor motor2(30000, 2, 8, 33, 32, 25);
 
 void setup() {
-  // sets the pins as outputs:
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
-
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
-  pinMode(enable2Pin, OUTPUT);
-  
-  // configure LED PWM functionalitites
-  // ledcSetup(pwmChannel, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  // ledcAttachPin(enable1Pin, pwmChannel);
-
   Serial.begin(115200);
+
+  motor.init();
+  motor2.init();
 
   // testing
   Serial.print("Testing DC Motor...");
 }
 
 void loop() {
-  // Move the DC motor forward at maximum speed
-  Serial.println("Moving Forward motor 1");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH); 
+  motor.forward();
+  motor2.forward();
+  delay(1000);
+
+  motor.stop();
+  motor2.stop();
   
-  analogWrite(enable1Pin, 255);
-
-  delay(2000);
-
-  Serial.println("stop motor 1");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW); 
-  analogWrite(enable1Pin, 0);
-  delay(5000);
-
-  Serial.println("Moving Forward motor 2");
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH); 
-  
-  analogWrite(enable2Pin, 255);
-
-  delay(2000);
-
-  Serial.println("stop motor 2");
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW); 
-  analogWrite(enable2Pin, 0);
   delay(5000);
 }

947dd250b369f9db183e41f9bf681fb258623fee
saidPolancoUPV <spolancom@upv.edu.mx>
2022-09-27 12:36:39 -0500

    actualización al sistema

--- sistemaRiegoAutomatico_basadoEnHumedadTierra/src/main.cpp
+++ sistemaRiegoAutomatico_basadoEnHumedadTierra/src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
+#include "pins_definition.h"
+#include "CWiFi.h"
+#include <PubSubClient.h>
 #include <DHT_U.h>
 #include <DHT.h>
 #include "CMotor.h"
 
-#define DHT_SENSOR_TYPE DHT22
-
 /****************** Definición de constantes ***********************/
+const char *ssid = "los odio a todos";
+const char *passwd = "p1ch1C0ntr453n4F4@";
+// const char *mqtt_server = "192.168.0.106";
+IPAddress mqttServer(192, 168, 0, 106);
+
+String list_of_topics[] = {"/cactuscare/ventilador", "/cactuscare/foco", "/cactuscare/placaterm", "/cactuscare/values/UV", "/cactuscare/values/humedad", "/cactuscare/values/temperatura", "/cactuscare/values/soil"};
+int num_of_topics = 7;
+
 int frequency = 30000;
 int resolution = 8;
 
-byte pinActivadorSoilSensor = 21;
-byte pinLecturaSoilSensor = 34;
-byte pinTempeaturaHumedadSensor = 19;
-byte pinUVSensor = 15;
-byte REF_3V3_UVSensor = 4;
+byte pinActivadorSoilSensor = 23;
+byte pinLecturaSoilSensor = 36;
+byte pinUVSensor = 39;
+byte REF_3V3_UVSensor = 34;
 
 float valorDisparoEncendidoMotor = 0.5; // [0, 1] valor entre 0 y 1 para el encendido del motor.
 /*******************************************************************/
@@ -26,12 +34,20 @@ void manejoBombaInundacion(float valorHumedad);
 void estableceTiempoSensoHumedad(long nuevoValor);
 int averageAnalogRead(int pinToRead);
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max);
+
+void reconnectMQTT();
+void callback(char* topic, byte* message, unsigned int length);
 /*******************************************************************/
 
 /******************* Definición de variables globales **************/
-DHT dht_sensor(pinTempeaturaHumedadSensor, DHT_SENSOR_TYPE);
-CMotor motorInundacion(frequency, 0, resolution, 27, 26, 14);
-CMotor motor2(frequency, 2, resolution, 33, 32, 25);
+
+CWiFi wifi(ssid, passwd); // iniciamos el objeto CWiFi
+WiFiClient espClient;
+PubSubClient mqttClient(mqttServer, 1883, callback, espClient);
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+CMotor motorInundacion(frequency, 0, resolution, MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_ENABLE_PIN);
+CMotor motorNutrientes(frequency, 2, resolution, MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_ENABLE_PIN);
 
 unsigned long tiempoPrevioSoilSensor = 0;
 unsigned long tiempoActualSoilSensor;
@@ -44,15 +60,26 @@ float valorTemperaturaAmbiente;
 
 void setup() {
   Serial.begin(115200);
+  wifi.init();
   dht_sensor.begin();
   motorInundacion.init();
 
   pinMode(pinActivadorSoilSensor, OUTPUT);
   pinMode(pinUVSensor, INPUT);
   pinMode(REF_3V3_UVSensor, INPUT);
+
+  pinMode(VENTILADOR_PIN, OUTPUT);
+  pinMode(FOCO_PIN, OUTPUT);
+  pinMode(PLACA_TERMICA_PIN, OUTPUT);
 }
 
 void loop() {
+
+  if (!mqttClient.connected()) {
+    reconnectMQTT();
+  }
+  mqttClient.loop();
+
   if (tiempoDeEspera(tiempoPrevioSoilSensor, tiempoEsperaSoilSensor)) {
     valorHumedadTierra = calculaPorcentajeDeHumedad(obtenValorSoilSensor());
     Serial.print("Porcentaje de humedad: ");
@@ -90,7 +117,17 @@ void loop() {
     Serial.print(" / UV Intensity (mW/cm^2): ");
     Serial.print(uvIntensity);
     Serial.println();
+
+    mqttClient.publish("/cactuscare/values/UV", String(uvIntensity).c_str());
+    delay(100);
+    mqttClient.publish("/cactuscare/values/humedad", String(valorHumedadAmbiente).c_str());
+    delay(100);
+    mqttClient.publish("/cactuscare/values/temperatura", String(valorTemperaturaAmbiente).c_str());
+    delay(100);
+    mqttClient.publish("/cactuscare/values/soil", String(valorHumedadTierra).c_str());
+    delay(100);
   }
+  wifi.reconexion();
 }
 
 void manejoBombaInundacion(float valorHumedad) {
@@ -152,4 +189,83 @@ int averageAnalogRead(int pinToRead) {
 
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+    /* Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: "); */
+    String messageTemp;
+
+    for (int i=0; i < length; i++) {
+         // Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+
+    if (String(topic) == "/cactuscare/ventilador") {
+      if (messageTemp == "ON") {
+        digitalWrite(VENTILADOR_PIN, HIGH);
+      }
+
+      if (messageTemp == "OFF") {
+        digitalWrite(VENTILADOR_PIN, LOW);
+      }
+    }
+
+    if (String(topic) == "/cactuscare/foco") {
+      if (messageTemp == "ON") {
+        digitalWrite(FOCO_PIN, HIGH);
+      }
+
+      if (messageTemp == "OFF") {
+        digitalWrite(FOCO_PIN, LOW);
+      }
+    }
+
+    if (String(topic) == "/cactuscare/placaterm") {
+      if (messageTemp == "ON") {
+        digitalWrite(PLACA_TERMICA_PIN, HIGH);
+      }
+
+      if (messageTemp == "OFF") {
+        digitalWrite(PLACA_TERMICA_PIN, LOW);
+      }
+    }
+
+    // aquí se ponen los eventos que se quieran manejar
+    /*********************** Ejemplo **********************************************************/
+    /*
+        if (String(topic) == "esp32/output") {
+            Serial.print("Changing output to ");
+            if(messageTemp == "on"){
+                Serial.println("on");
+                digitalWrite(ledPin, HIGH);
+            } else if(messageTemp == "off"){
+                Serial.println("off");
+                digitalWrite(ledPin, LOW);
+            }
+        }
+    */
+    /******************************************************************************************/
+}
+
+void reconnectMQTT() {
+    // clico hasta que este conectado
+    while (!mqttClient.connected()) {
+        Serial.print("Conectando a Servidor MQTT...");
+        if (mqttClient.connect("ESP32Client", "spolancom", "m15cuy1t0s")) {
+            Serial.println("connected");
+            // subscribe
+            // mqttClient.subscribe("/esp32/test");
+
+            for (byte i=0; i < num_of_topics; i++) {
+                mqttClient.subscribe(list_of_topics[i].c_str());
+            }
+        } else {
+            Serial.print("Error en la conección, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" intentando en 5 segundos");
+            delay(5000);
+        }
+    }
 }
---
+++ sistemaRiegoAutomatico_basadoEnHumedadTierra/src/pins_definition.h
@@ -0,0 +1,21 @@
+#ifndef PINS_DEFINITION_H
+#define PINS_DEFINITION_H
+
+// MTORO 1
+#define MOTOR_1_ENABLE_PIN 13
+#define MOTOR_1_PIN_1 12
+#define MOTOR_1_PIN_2 14
+// MOTOR 2
+#define MOTOR_2_ENABLE_PIN 27
+#define MOTOR_2_PIN_1 26
+#define MOTOR_2_PIN_2 25
+
+// SENSOR DHT- TEMPERATURA/HUMEDAD
+#define DHT_SENSOR_TYPE DHT22
+#define DHT_SENSOR_PIN 33
+
+// modulo de 4 relays
+#define VENTILADOR_PIN 22
+#define FOCO_PIN 21
+#define PLACA_TERMICA_PIN 19
+#endif
